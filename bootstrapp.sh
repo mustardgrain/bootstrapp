7a0c9718b347a7d57aa2a50d90e64017251c41d4
Kirk True <kirk@mustardgrain.com>
2015-05-27 21:35:09 -0700

    Added Liquibase, Play\!, and Scala

--- bootstrapp.sh
+++ bootstrapp.sh
@@ -20,13 +20,19 @@ elif [ `uname` = 'Darwin' ] ; then
   GO_URL=http://golang.org/dl/go${GO_VERSION}.darwin-amd64-osx10.8.tar.gz
 fi
 
+LIQUIBASE_VERSION=3.2.3
+LIQUIBASE_URL=http://downloads.sourceforge.net/project/liquibase/Liquibase%20Core/liquibase-$LIQUIBASE_VERSION-bin.zip
+
 MAVEN_VERSION=3.2.5
 MAVEN_URL=$APACHE_MIRROR_URL/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
+
+PLAY_VERSION=2.2.3
+PLAY_URL=http://downloads.typesafe.com/play/$PLAY_VERSION/play-$PLAY_VERSION.zip
 
 RDS_CLI_VERSION="(latest)"
 RDS_CLI_URL=$AMAZON_MIRROR_URL/rds-downloads/RDSCli.zip
 
-SCALA_VERSION=2.10.4
+SCALA_VERSION=2.10.3
 SCALA_URL=http://downloads.typesafe.com/scala/$SCALA_VERSION/scala-$SCALA_VERSION.tgz
 
 function untar_download() {
@@ -69,17 +75,24 @@ function bootstrap() {
 }
 
 function bootstrap_emr_cli() {
-  URL=$1
-
   rm -rf emr-cli
   mkdir emr-cli
   cd emr-cli
 
-  unzip_download $URL
+  unzip_download $EMR_CLI_URL
 
   cd ..
 }
 
+function bootstrap_liquibase() {
+  rm -rf liquibase
+  mkdir liquibase
+  cd liquibase
+
+  unzip_download $LIQUIBASE_URL
+  chmod +x liquibase
+  cd ..
+}
 
 function usage() {
   WIDTH=20
@@ -91,8 +104,11 @@ function usage() {
   echo "  ec2-api-tools     $(printf %${WIDTH}s $EC2_API_TOOLS_VERSION) $EC2_API_TOOLS_URL"
   echo "  emr-cli           $(printf %${WIDTH}s $EMR_CLI_VERSION) $EMR_CLI_URL"
   echo "  go                $(printf %${WIDTH}s $GO_VERSION) $GO_URL"
+  echo "  liquibase         $(printf %${WIDTH}s $LIQUIBASE_VERSION) $LIQUIBASE_URL"
   echo "  maven             $(printf %${WIDTH}s $MAVEN_VERSION) $MAVEN_URL"
+  echo "  play              $(printf %${WIDTH}s $PLAY_VERSION) $PLAY_URL"
   echo "  rds-cli           $(printf %${WIDTH}s $RDS_CLI_VERSION) $RDS_CLI_URL"
+  echo "  scala             $(printf %${WIDTH}s $SCALA_VERSION) $SCALA_URL"
 
   exit 1
 }
@@ -107,13 +123,19 @@ for download in "$@" ; do
   elif [ "$download" = "ec2-api-tools" ] ; then
     bootstrap ec2-api-tools ec2-api-tools- $EC2_API_TOOLS_URL
   elif [ "$download" = "emr-cli" ] ; then
-    bootstrap_emr_cli $EMR_CLI_URL
+    bootstrap_emr_cli
   elif [ "$download" = "go" ] ; then
     bootstrap "" go $GO_URL
+  elif [ "$download" = "liquibase" ] ; then
+    bootstrap_liquibase
   elif [ "$download" = "maven" ] ; then
     bootstrap maven apache-maven $MAVEN_URL
+  elif [ "$download" = "play" ] ; then
+    bootstrap play play $PLAY_URL
   elif [ "$download" = "rds-cli" ] ; then
     bootstrap rds-cli RDSCli- $RDS_CLI_URL
+  elif [ "$download" = "scala" ] ; then
+    bootstrap scala scala $SCALA_URL
   else
     usage
   fi
